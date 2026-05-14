0b4b86c2f9ab9e8fa0cdc4f8cac31c8bd75c5dec
xxMichalPk <michal10.05.pk@gmail.com>
2026-05-14 15:31:38 +0200

    Improve Pair implementation

--- include/Pair.hpp
+++ include/Pair.hpp
@@ -5,13 +5,16 @@
 
 template<typename T1, typename T2>
 class Pair {
-    public:
+    protected:
         T1 element1 {};
         T2 element2 {};
 
     public:
         Pair() = default;
         Pair(T1 e1, T2 e2) : element1(e1), element2(e2) {}
+
+        T1 first() { return element1; }
+        T2 second() { return element2; }
 
         bool operator<(Pair e2) { return element1 < e2.element1; }
         bool operator<=(Pair e2) { return element1 <= e2.element1; }
@@ -22,7 +25,7 @@ class Pair {
 
 template<typename T1, typename T2>
 std::ostream& operator<<(std::ostream& os, const Pair<T1, T2>& p) { 
-    os << "{" << p.element1 << ", " << p.element2 << "}";
+    os << "{" << p.first() << ", " << p.second() << "}";
     return os;
 }
 
