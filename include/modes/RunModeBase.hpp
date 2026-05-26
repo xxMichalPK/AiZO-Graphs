46eb68fa4f851a145e051fd411977417f84e79fc
xxMichalPk <michal10.05.pk@gmail.com>
2026-05-26 08:42:14 +0200

    Remove unnecessary comments

--- include/modes/RunModeBase.hpp
+++ include/modes/RunModeBase.hpp
@@ -14,27 +14,12 @@ class RunModeBase {
         RunModeBase() = delete;
         virtual ~RunModeBase() = default;
 
-        /**
-         * Tells you if the graph should be directed or not
-         * 
-         * @returns true if it should be directed, false otherwise
-         */
         static bool isDirected();
 
-        /**
-         * Creates all requested graph representations
-         * 
-         * @param vertexCount number of vertices in the graph
-         * @param edgeCount number of edges in the graph
-         * 
-         * @returns a dynamic array of graph representations, nullptr on failure
-         */
         static DynamicArray<GraphRepr*>* createRepresentations(size_t vertexCount, size_t edgeCount);
-
         static void deleteRepresentations(DynamicArray<GraphRepr*>* representations);
 
         static DynamicArray<GraphAlgorithmBase*>* createAlgorithms(GraphRepr& graph);
-
         static void deleteAlgorithms(DynamicArray<GraphAlgorithmBase*>* algorithms);
     
     private:
